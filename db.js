b21da69d6260068f5049096abbb566cab1d1b833
Igor Cavliuc <cavliuc.sh@inbox.ru>
2023-06-02 21:21:31 +0300

    ...

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://cavliucserv:zXSCKfTxa1YGFVnu@database.uppj1fv.mongodb.net/?retryWrites=true&w=majority";
+// const URL = "mongodb+srv://cavliucserv:zXSCKfTxa1YGFVnu@database.uppj1fv.mongodb.net/?retryWrites=true&w=majority";
+const URL =  "mongodb+srv://cavliucserv:zXSCKfTxa1YGFVnu@database.uppj1fv.mongodb.net/Batman?retryWrites=true&w=majority"
+
 let dbConnection;
 
 module.exports = {
-    connectToDb: async (cb) => {
-        try {
-            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            await client.connect();
-            console.log("Connected to MongoDB");
-            dbConnection = client.db();
-            cb();
-        } catch (err) {
-            cb(err);
-        }
+    connectToDb: (cb) => {
+        MongoClient.connect(URL)
+            .then((client) => {
+                console.log(client);
+                dbConnection = client.db();
+                return cb();
+            })
+            .catch((err) => {
+                return cb(err);
+            });
     },
     getDb: () => dbConnection,
 };
